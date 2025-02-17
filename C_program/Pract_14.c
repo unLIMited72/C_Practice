6adce7f910c5206ebc6bab34d4b6ef39e60ec129
Lim <LimCraft7260@gmail.com>
2025-02-17 15:43:14 +0900

    Practice C/Dynamic Memory Allocation a.k.a malloc

---
+++ C_program/Pract_14.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+
+    int* pList = NULL;
+    pList = (int*)malloc(sizeof(int)*3);
+
+    pList[0] = 10;
+    pList[1] = 20;
+    pList[2] = 30;
+
+    for (int i = 0; i < 3; i++)
+        printf("%d\n", pList[i]);
+    
+    free(pList);
+}
